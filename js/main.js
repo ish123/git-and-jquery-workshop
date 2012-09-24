ba83bf68ca808c7be97028ee55a5e2a2df6135f6
Iswarya <hocker_iswarya@hotmail.com>
2012-09-23 21:15:55 -0700

    I'm a jquery ninja

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@
 // $("img").width(200);
 
 $(document).ready(function(){
+	$("li").on("click",function(){
+
+		if($(this).hasClass("done")){
+			$(this).removeClass("done")
+		}else{
+
+		$(this).addClass("done") //says what is clicked
+		//$("li").addClass("done")
+
+		}
+	});
+
+
 $("img").on("dblclick", function(){
 //img on click same as img click code below
 	// here.
---
+++ main.js
@@ -0,0 +1,10 @@
+// console.log("main.js is loaded");
+// $("img").width(200);
+
+$(document).ready(function(){
+$("img").click(function(){
+	// here.
+	$("img").width(200);
+
+	});
+});
