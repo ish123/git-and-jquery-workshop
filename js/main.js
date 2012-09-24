5f27ce1997bdefb22c5366de424d9f04991e42e1
Iswarya <hocker_iswarya@hotmail.com>
2012-09-23 20:49:29 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// console.log("main.js is loaded");
+// $("img").width(200);
+
+$(document).ready(function(){
+$("img").on("dblclick", function(){
+//img on click same as img click code below
+	// here.
+	$("img").width(200);
+	$("h1").fadeOut();
+	$(".booboo").slideUp();
+	$("#rahul").hide();
+});
+
+$("img").click(function(){
+	// here.
+	// $("img").width(200);
+
+	});
+});
